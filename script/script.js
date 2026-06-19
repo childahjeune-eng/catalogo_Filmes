181484444670b0baa7b7d4f004c6a0502a7b4b55
childah.Al.jeune <childah.jeune@escola.pr.gov.br>
2026-06-19 11:18:54 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,34 +1,8 @@
-const filmes = [{
-    titulo: "Vingadores",
-    imagem: "https://noticiasdatv.uol.com.br/media/_versions/ordem-cronologica-marvel-vingadores-ultimato-imagem-divulgacao-disney_fixed_large.jpg",
-    descricao: "Os maiores heróis da terra unidos",
-},
-{
-    titulo: "Batman",
-    imagem:"https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg",
-    descricao: "O cavaleiro das trevas"
-},
-{
-    titulo: "Homem-Aranha",
-    imagem: "https://image.tmdb.org/t/p/w500/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg",
-    descricao: "O amigo da vizinhança"
-},
-{
-    titulo: "Aladdin",
-    imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/b/be/Aladdin_%282019%29.jpg/250px-Aladdin_%282019%29.jpg",
-    descricao: "lâmpada mágica"
-},
-{
-    titulo: "Harry Potter",
-    imagem: "https://blog.pensanddolls.com.br/wp-content/uploads/2023/03/4-fatos-que-voce-nao-sabe-sobre-Harry-Potter.jpg",
-    descricao: "Pedra Filosofal"
-},
-{
-    titulo: "Esqueceram de mim",
-    imagem: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/096d1213-70ba-412a-b09f-8f2555f1061f/compose?aspectRatio=1.78&format=webp&width=1200",
-    descricao: "Uma grande aventura de natal"
+async function carregarFilmes() {
+    const resposta = await fetch("https://localhost:5000/filmes")
+    const filmes = await resposta.json()
+    mostrarFilmes(filmes)
 }
-]
 
 const listaFilmes = document.getElementById ("listaFilmes");
 
@@ -50,7 +24,7 @@ function mostrarFilmes(lista) {
     })
 }
 
-mostrarFilmes(filmes)
+carregarFilmes()
 function verDetalhes(titulo, descricao) {
     Swal.fire({
         title: titulo,
